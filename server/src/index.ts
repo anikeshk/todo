d45398d962d7d2379d4f3a8475c477c82aaae201
anikeshk <anikesh.kamath@gmail.com>
2024-05-29 08:39:22 -0400

    Add log line for server start

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,4 +3,6 @@ dotenv.config();
 
 import app from './app';
 
-app.listen(process.env.SERVER_PORT);
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server running on port ${process.env.SERVER_PORT}`);
+});
